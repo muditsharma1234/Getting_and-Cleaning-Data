e193052dcc6ef2437fe6cb34ba168b54db0141c7
muditsharma1234 <mudit.bits@gmail.com>
2017-05-25 14:11:51 +0530

    Run this file to generate output html and tidy ds

--- run_analysis.r
+++ run_analysis.r
@@ -1,5 +1,7 @@
-install.packages("knitr")
-install.packages("markdown")
+required_packages <- c("knitr","markdown")
+install_required <- setdiff(required_packages,rownames(installed.packages()))
+if(length(install_required)!=0) {install.packages(install_required)}
+
 ##This file is primarily used to execute the rmd file to generate the html format
 ##Most of the actual coding is done in the rmd file along with html file
 require(knitr)
